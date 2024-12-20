1772f76a38f5c34bedbb077441b856adaf9b2239
sadek1999 <sadekali1530@gmail.com>
2024-12-20 08:16:45 +0600

    create bookings store ,update and edit controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class BookingController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
     public function index()
     {
-        //
+        $data=Booking::latest()->paginate();
+        return Inertia::render('Booing/index',[
+            'bookings'=>$data
+        ]);
     }
 
     /**
@@ -28,7 +34,18 @@ class BookingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validateData = $request->validate([
+            'room_id' => 'required|integer|exists:rooms,id', // Ensure room ID exists in the rooms table as a primary key
+            'start_time' => 'required|date_format:H:i', // Validate start time in HH:mm format
+            'end_time' => 'required|date_format:H:i|after:start_time', // End time must be after start time
+            'date' => 'required|date|after_or_equal:today', // Ensure date is today or in the future
+
+        ]);
+        $validateData['user_id']=Auth::user()->id;
+
+        Booking::create($validateData);
+
+        return back();
     }
 
     /**
@@ -36,7 +53,9 @@ class BookingController extends Controller
      */
     public function show(Booking $booking)
     {
-        //
+        return Inertia::render('Booking/show',[
+            'booking'=>$booking
+        ]) ;
     }
 
     /**
@@ -44,7 +63,9 @@ class BookingController extends Controller
      */
     public function edit(Booking $booking)
     {
-        //
+        return Inertia::render('Booking/edit',[
+            'booking'=>$booking
+        ]);
     }
 
     /**
@@ -52,14 +73,26 @@ class BookingController extends Controller
      */
     public function update(Request $request, Booking $booking)
     {
-        //
+        if($booking->user_id!==Auth::user()->id){
+            return redirect()->back()->withErrors(['error'=>'you are not authorized for update this bookings']);
+        };
+        $validateData=$request->validate([
+           'room_id' => 'integer|exists:rooms,id',
+            'start_time' => 'date_format:H:i',
+            'end_time' => 'date_format:H:i|after:start_time',
+            'date' => 'date|after_or_equal:today',
+        ]);
+        $booking->update($validateData);
+        return redirect()->route('index')->with('success','successfully update the booking');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Booking $booking)
-    {
-        //
+    {$authUser=Auth::user()->id;
+        if()
+
     }
 }
